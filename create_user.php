2f63c327905a234b6f23685852ff4881a4f7adba
ScottyNong <scotty.nong@yahoo.fr>
2025-02-19 18:56:08 +0100

    Update create_user.php

--- create_user.php
+++ create_user.php
@@ -7,7 +7,7 @@ $data = json_decode(file_get_contents("php://input"), true);
 
 if (isset($data['email']) && isset($data['password'])) {
     $email = $data['email'];
-    $password = hash("sha256", $data['password'])
+    $password = hash("sha256", $data['password']);
 
     // Préparer les données pour l'envoi au script Google Apps
     $postData = json_encode([
